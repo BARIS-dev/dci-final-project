2f051c53cad5a8172a743b104db83055f56903d5
Kim Anh Do <kim@Kims-MBP.fritz.box>
2024-01-08 10:42:01 +0100

    Added not-found handler and error handler

--- FitFrenzy/server/index.js
+++ FitFrenzy/server/index.js
@@ -21,6 +21,24 @@ app.use(express.json());
 app.use("/user", userRouter);
 app.use("products", productsRouter);
 
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    answer: {
+      code: 404,
+      message: "Page not found",
+    },
+  });
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.code || 500).json({
+    answer: {
+      code: error.code || 500,
+      message: error.message || "Internal server error",
+    },
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
