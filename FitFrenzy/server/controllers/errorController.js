b5e58a457056bc709d9b5b9d30413bfdf8cb2b75
omerfkgoze <farukucukgoze@gmail.com>
2024-01-10 12:02:20 +0100

    added regular expression for error handling

--- FitFrenzy/server/controllers/errorController.js
+++ FitFrenzy/server/controllers/errorController.js
@@ -7,8 +7,8 @@ const handleCastErrorDB = err => {
 };
 
 const handleDuplicateFieldsDB = err => {
-  // const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  const value = err.keyValue.name;
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
   const message = `Duplicate field value: ${value}. Please use another value!`;
 
   return new AppError(message, 400);
