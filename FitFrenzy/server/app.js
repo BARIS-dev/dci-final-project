0a29973cb6d1c72c7fea4df8311087d5b0d7bb87
omerfkgoze <farukucukgoze@gmail.com>
2024-01-10 11:46:03 +0100

    installed morgan and implemented error handling

--- FitFrenzy/server/app.js
+++ FitFrenzy/server/app.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import userRouter from './routes/user.route.js';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
 import AppError from './utils/appError.js';
+import globalErrorHandler from './controllers/errorController.js';
+import userRouter from './routes/user.route.js';
+
+dotenv.config();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use('/user', userRouter);
@@ -11,5 +20,7 @@ app.use('/user', userRouter);
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 export default app;
--- FitFrenzy/server/server.js
+++ FitFrenzy/server/server.js
@@ -7,6 +7,15 @@ import {
 } from './config/db.connect.js';
 import app from './app.js';
 
+// handle uncaught exceptions
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+
+  process.exit(1); // 0 success, 1 uncaught exception
+});
+
+// connect to DB
 config();
 mongoErrorListener();
 mongoConnectListener();
@@ -15,4 +24,15 @@ await mongoConnect();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+// handle unhandled rejections
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+
+  // giving time to finish all the requests
+  server.close(() => {
+    process.exit(1); // 0 success, 1 unhandled rejection
+  });
 });
