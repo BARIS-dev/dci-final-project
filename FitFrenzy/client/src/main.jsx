8136db21ea01fb8b0c8d0e421773a3dbae3e5321
Andreea Fleancu <andreea.enigma@gmail.com>
2024-01-15 11:19:56 +0100

    update

--- FitFrenzy/client/src/main.jsx
+++ FitFrenzy/client/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { UserContextProvider } from './context/user.context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
