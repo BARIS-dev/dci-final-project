41e657c16bfc7d937fbe6f36a89b1f12639a03e3
dci-student <zizi.0400@gmail.com>
2024-01-15 10:01:08 +0100

    update

--- FitFrenzy/server/middlewares/validators/username.validator.js
+++ FitFrenzy/server/middlewares/validators/username.validator.js
@@ -1,14 +1,21 @@
 import expressvalidator from "express-validator";
 
 export const usernameValidator = expressvalidator
+
   .check("username")
-  .length({ min: 3, max: 30 })
+
+  .isLength({ min: 3, max: 30 })
+
   .withMessage("Der Benutzername muss zwischen 3 und 50 Zeichen lang sein")
+
   .trim()
+
   .escape()
+
   .custom((value) => {
     if (value.toLowerCase() === "admin") {
       throw new Error("Der Benutzername darf nicht 'Admin' sein");
     }
+
     return true;
   });
