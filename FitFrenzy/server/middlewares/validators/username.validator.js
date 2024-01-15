96f57c54cc4621b1b1088c8188e10a906fbd2931
Baris-dev <barishakan.dev@gmail.com>
2024-01-15 09:21:59 +0100

    Small hotfix

--- FitFrenzy/server/middlewares/validators/username.validator.js
+++ FitFrenzy/server/middlewares/validators/username.validator.js
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-import expressvalidator from "express-validator";
-
- 
-
-export const usernameValidator = expressvalidator
-
-  .check("username")
-<<<<<<< HEAD
-  .isLength({ min: 3, max: 30 })
-=======
-
-  .isLength({ min: 3, max: 30 })
-
->>>>>>> main
-  .withMessage("Der Benutzername muss zwischen 3 und 50 Zeichen lang sein")
-
-=======
 import { check } from "express-validator";
+
 export const usernameValidator = check("username")
   .isLength({ min: 2, max: 30 })
   .withMessage("Der Name muss zwischen 2 und 50 Zeichen lang sein")
->>>>>>> main
   .trim()
   .escape()
   .custom((value) => {
