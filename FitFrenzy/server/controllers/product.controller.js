2ebcbe9cabe0c048be515106c35702ef9c575cd0
Kim Anh Do <kim@Kims-MBP.fritz.box>
2024-01-18 16:05:56 +0100

    Updated addProductToCart controller and installed cookie parser

--- FitFrenzy/server/controllers/product.controller.js
+++ FitFrenzy/server/controllers/product.controller.js
@@ -263,33 +263,122 @@ export async function toggleLikeController(req, res, next) {
 
 export async function addProductToCartController(req, res, next) {
   const { productId } = req.params;
-  //const { quantity } = req.body;?
-
-  const username = req.user.username;
-  //user need to login first?
 
   try {
-    const userCart = await cartModel.findOne({ username: username });
+    //Check if user already logged in
+    const username = req.user.username;
 
-    if (userCart) {
-      const itemInCart = userCart.items.productId.includes(productId);
+    //AUTHENTICATED USER
+    if (username) {
+      //if user already logged in => check if user already has a cart
+      const usersCart = await cartModel.findOne({ username: username });
 
-      if (itemInCart) {
-        const updatedQuantityCart = await cartModel.findOneAndUpdate(
-          { username: username, "items.productId": productId },
-          { $inc: { "items.quantity": 1 } },
-          { new: true }
+      if (usersCart) {
+        //if user already has a cart => check if product already in cart
+        const productAlreadyInCart =
+          usersCart.items.productId.includes(productId);
+
+        if (productAlreadyInCart) {
+          //product already in cart => increase quantity
+          const updatedQuantityCart = await cartModel.findOneAndUpdate(
+            { username: username, "items.productId": productId },
+            { $inc: { "items.quantity": 1 } },
+            { new: true }
+          );
+          res.status(200).json({
+            answer: {
+              code: 200,
+              message: "Produktmenge im Warenkorb aktualisiert",
+              data: updatedQuantityCart,
+            },
+          });
+        } else {
+          //product not in cart yet => add product to cart (quantity 1)
+          const updatedCart = await cartModel.findOneAndUpdate(
+            { username: username },
+            { $addToSet: { items: { productId: productId, quantity: 1 } } },
+            { new: true }
+          );
+          res.status(200).json({
+            answer: {
+              code: 200,
+              message: "Produkt wurde zum Warenkorb hinzugefügt",
+              data: updatedCart,
+            },
+          });
+        }
+      } else {
+        //if user has no cart yet => create new cart and add product to cart (quantity 1)
+        const newCart = cartModel.create({
+          username: username,
+          items: [{ productId: productId, quantity: 1 }],
+        });
+        await newCart.save();
+        res.status(200).json({
+          answer: {
+            code: 200,
+            message: "Warenkorb erstellt und Produkt hinzugefügt",
+            data: newCart,
+          },
+        });
+      }
+    }
+
+    //NON-AUTHENTICATED USER (GUEST)
+    else {
+      //if user not logged in => check if user has a (guest)Cart in cookie
+      const guestCart = req.cookies.guestCart;
+
+      if (guestCart) {
+        //if user has a guestCart => check if product already in cart
+        const guestCartObj = JSON.parse(guestCart);
+        const productAlreadyInGuestCart = guestCartObj.items.some(
+          (item) => item.productId === productId
         );
+
+        if (productAlreadyInGuestCart) {
+          //if product already in guestCart  => increase quantity in this guest cart in cookie
+          const updatedQuantityGuestCart = guestCartObj.items.map((item) => {
+            if (item.productId === productId) {
+              item.quantity += 1;
+            }
+            return item;
+          });
+
+          //update the guestCart in cookie
+          res.cookie(
+            "guestCart",
+            JSON.stringify({ items: updatedQuantityGuestCart }),
+            { maxAge: 86400000 }
+          );
+        } else {
+          //product not in guestCart yet => add product to cart (quantity 1)
+          guestCartObj.items.push({ productId: productId, quantity: 1 });
+
+          //update the guestCart in cookie
+          res.cookie("guestCart", JSON.stringify(guestCartObj), {
+            maxAge: 86400000,
+          });
+        }
+      } else {
+        //if user has no guestCart in localStorage/cookie yet => create new guestCart and add product to cart (quantity 1)
+        const newGuestCart = {
+          items: [{ productId: productId, quantity: 1 }],
+        };
+
+        //save guestCart in cookie
+        res.cookie("guestCart", JSON.stringify(newGuestCart), {
+          maxAge: 86400000,
+        });
 
         res.status(200).json({
           answer: {
             code: 200,
-            message: "Produktmenge im Warenkorb aktualisiert",
-            data: updatedQuantityCart,
+            message: "Gast-Warenkorb erstellt und Produkt hinzugefügt",
+            data: newGuestCart,
           },
         });
       }
-    } else {
     }
   } catch (error) {
     console.log(error);
