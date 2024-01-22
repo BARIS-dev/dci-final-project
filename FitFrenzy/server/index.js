0941c490ab75ff9ee365b9052ceff5c73c20ba43
BARIŞ <barishakan.dev@gmail.com>
2024-01-22 13:45:00 +0100

    Added payment gateway (Stripe)

--- FitFrenzy/server/index.js
+++ FitFrenzy/server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { config } from "dotenv";
+import cors from "cors";
 import {
   mongoConnect,
   mongoConnectListener,
@@ -9,6 +10,7 @@ import {
 import { userRouter } from "./routes/user.route.js";
 import { productRouter } from "./routes/product.route.js";
 import { favoriteRouter } from "./routes/favorite.route.js";
+import stripeRouter from "./routes/payment.route.js";
 
 config();
 mongoErrorListener();
@@ -18,10 +20,11 @@ await mongoConnect();
 
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use("/user", userRouter);
 app.use("/", productRouter);
 app.use("/favorites", favoriteRouter);
+app.use("/payment", stripeRouter);
 
 app.all("*", (req, res, next) => {
   res.status(404).json({
