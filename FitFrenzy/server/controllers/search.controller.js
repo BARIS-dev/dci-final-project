3c83ba88a9f8f3f488e89e5762e54d26e18ab5f4
Kim Anh Do <kimanh188.de@gmail.com>
2024-01-24 11:51:57 +0100

    Updated catchAsync for controllers

--- FitFrenzy/server/controllers/search.controller.js
+++ FitFrenzy/server/controllers/search.controller.js
@@ -1,26 +1,22 @@
 import productModel from "../models/product.model.js";
+import catchAsync from "../utils/catchAsync.js";
 
-export async function searchController(req, res, next) {
+export const searchController = catchAsync(async (req, res, next) => {
   const { searchValue } = req.query;
 
-  try {
-    const matchedProducts = await productModel.find({
-      $or: [
-        { name: { $regex: searchValue, $options: "i" } },
-        { description: { $regex: searchValue, $options: "i" } },
-        { category: { $regex: searchValue, $options: "i" } },
-      ],
-    });
+  const matchedProducts = await productModel.find({
+    $or: [
+      { name: { $regex: searchValue, $options: "i" } },
+      { description: { $regex: searchValue, $options: "i" } },
+      { category: { $regex: searchValue, $options: "i" } },
+    ],
+  });
 
-    res.status(200).json({
-      answer: {
-        code: 200,
-        message: `${matchedProducts.length} Ergebnisse gefunden`,
-        data: matchedProducts,
-      },
-    });
-  } catch (error) {
-    console.log(error);
-    //next();
-  }
-}
+  res.status(200).json({
+    answer: {
+      code: 200,
+      message: `${matchedProducts.length} Ergebnisse gefunden`,
+      data: matchedProducts,
+    },
+  });
+});
