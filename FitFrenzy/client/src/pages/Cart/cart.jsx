f8dc15eaa23d62119eea559e195dcc42302ae257
kim <kimanh188.de@gmail.com>
2024-02-15 16:10:00 +0100

    Fix the git error

--- FitFrenzy/client/src/pages/Cart/cart.jsx
+++ FitFrenzy/client/src/pages/Cart/cart.jsx
@@ -1,16 +1,9 @@
-import "./cart.css";
-import { useContext } from "react";
-import { CartContext } from "../../context/cart.context.jsx";
 
-const Cart = () => {
-  const { cart } = useContext(CartContext);
 
+function Cart() {
   return (
-    <section>
-      <h1>CART</h1>
-      <button onClick={() => console.log(cart)}>Show Cart in console</button>
-    </section>
-  );
-};
+    <div>Cart</div>
+  )
+}
 
-export default Cart;
+export default Cart
