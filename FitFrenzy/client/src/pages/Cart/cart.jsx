77f6d08e1dc197be3f556ed01546bc3fc3475f31
kim <kimanh188.de@gmail.com>
2024-02-15 16:17:59 +0100

    test

--- FitFrenzy/client/src/pages/Cart/cart.jsx
+++ FitFrenzy/client/src/pages/Cart/cart.jsx
@@ -1,9 +1,32 @@
-
+import "./cart.css";
+import { useContext } from "react";
+import { CartContext } from "../../context/cart.context.jsx";
 
 function Cart() {
+  const { cart } = useContext(CartContext);
+
   return (
-    <div>Cart</div>
-  )
+    <section>
+      {cart.length === 0 ? (
+        <h1>Your cart is empty</h1>
+      ) : (
+        <div>
+          {cart.items.map((item) => {
+            return (
+              <div key={item.productId}>
+                <p>{item.productName}</p>
+                <p>{item.quantity}</p>
+                <p>{item.productPrice}</p>
+                <p>{item.productSize}</p>
+                <p>{item.productColor}</p>
+              </div>
+            );
+          })}
+          <p>Total: {cart.sum}</p>
+        </div>
+      )}
+    </section>
+  );
 }
 
-export default Cart
+export default Cart;
