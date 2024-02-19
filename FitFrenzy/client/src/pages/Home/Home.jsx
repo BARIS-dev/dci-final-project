5e754af186d26795a77db5871e098c4172daf650
kim <kimanh188.de@gmail.com>
2024-02-19 11:31:22 +0100

    Fixed after pull

--- FitFrenzy/client/src/pages/Home/Home.jsx
+++ FitFrenzy/client/src/pages/Home/Home.jsx
@@ -1,14 +1,14 @@
 import "./Home.css";
 import HeroItem from "../../components/Hero/HeroItem.jsx";
 import BrandsBarItem from "../../components/BrandsBar/BrandsBarItem.jsx";
-import ProductsList from "../../components/shared/productsList/ProductsList";
+//import ProductsList from "../../components/shared/productsList/ProductsList";
 
 const Home = () => {
   return (
     <>
       <HeroItem />
       <BrandsBarItem />
-      <ProductsList />
+      {/* <ProductsList /> */}
     </>
   );
 };
