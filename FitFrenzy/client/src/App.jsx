a3e19f8d637854b17a1b72433d8af53e2e87f0b1
omerfkgoze <farukucukgoze@gmail.com>
2024-02-19 13:06:57 +0100

    started to Meine Bestellungen page

--- FitFrenzy/client/src/App.jsx
+++ FitFrenzy/client/src/App.jsx
@@ -1,22 +1,23 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home.jsx";
-import Account from "./pages/Account/Account.jsx";
-import Signin from "./pages/Signin/Signin.jsx";
-import { UserContextProvider } from "./context/user.context.jsx";
-import Payment from "./pages/Payment/Payment.jsx";
-import Footer from "./components/shared/footer/Footer.jsx";
-import AdminDashboard from "./pages/Admin/admin.dashboard.jsx";
-import ProductCreate from "./pages/Admin/product.create.jsx";
-import ProductDetail from "./pages/Product/product.detail.jsx";
-import Cart from "./pages/Cart/cartPage.jsx";
-import Signout from "./pages/Signout/Signout.jsx";
-import PaymentMethods from "./pages/PaymentMethods/PaymentMethods.jsx";
-import Checkout from "./pages/Checkout/Checkout.jsx";
-import RegisterPage from "./pages/Register/Register.jsx";
-import Navbar from "./components/shared/Navbar/Navbar.jsx";
-import Favorites from "./pages/Favorites/favorites.jsx";
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home.jsx';
+import Account from './pages/Account/Account.jsx';
+import Signin from './pages/Signin/Signin.jsx';
+import { UserContextProvider } from './context/user.context.jsx';
+import Payment from './pages/Payment/Payment.jsx';
+import Footer from './components/shared/footer/Footer.jsx';
+import AdminDashboard from './pages/Admin/admin.dashboard.jsx';
+import ProductCreate from './pages/Admin/product.create.jsx';
+import ProductDetail from './pages/Product/product.detail.jsx';
+import Cart from './pages/Cart/cartPage.jsx';
+import Signout from './pages/Signout/Signout.jsx';
+import PaymentMethods from './pages/PaymentMethods/PaymentMethods.jsx';
+import Checkout from './pages/Checkout/Checkout.jsx';
+import RegisterPage from './pages/Register/Register.jsx';
+import Navbar from './components/shared/Navbar/Navbar.jsx';
+import Favorites from './pages/Favorites/favorites.jsx';
 import ProductsList from './pages/ProductsList/ProductsList.jsx';
+import MyOrders from './pages/MyOrders/MyOrders.jsx';
 
 function App() {
   // test data from data folder
@@ -43,6 +44,7 @@ function App() {
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/signed-out" element={<Signout />} />
           <Route path="/account" element={<Account />} />
+          <Route path="/my-orders" element={<MyOrders />} />
           <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/cart" element={<Cart />} />
@@ -54,7 +56,6 @@ function App() {
           <Route path="*" element={<h1>Not Found</h1>} />
           <Route path="/checkout" element={<Checkout />} />
           <Route path="/products-list" element={<ProductsList />} />
-          
         </Routes>
         <Footer />
       </UserContextProvider>
