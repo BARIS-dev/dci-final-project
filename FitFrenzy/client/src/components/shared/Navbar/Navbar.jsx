32d77d3b75313a493a7d386638911b317cec20a1
omerfkgoze <farukucukgoze@gmail.com>
2024-02-21 09:17:10 +0100

    added hover effect to navbar

--- FitFrenzy/client/src/components/shared/Navbar/Navbar.jsx
+++ FitFrenzy/client/src/components/shared/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
-import "animate.css";
-import { Link } from "react-router-dom";
+import 'animate.css';
+import { Link } from 'react-router-dom';
 import {
   FaCartShopping,
   FaUser,
@@ -7,10 +7,10 @@ import {
   FaX,
   FaShop,
   FaHeart,
-} from "react-icons/fa6";
-import { FaSearch, FaPercentage } from "react-icons/fa";
-import { BiSolidCategory } from "react-icons/bi";
-import { FiPackage } from "react-icons/fi";
+} from 'react-icons/fa6';
+import { FaSearch, FaPercentage } from 'react-icons/fa';
+import { BiSolidCategory } from 'react-icons/bi';
+import { FiPackage } from 'react-icons/fi';
 
 import {
   SiNike,
@@ -18,15 +18,15 @@ import {
   SiPuma,
   SiReebok,
   SiUnderarmour,
-} from "react-icons/si";
+} from 'react-icons/si';
 
-import "./Navbar.css";
-import { useContext, useState } from "react";
-import Navbarchat from "../../chat/Navbarchat/NavbarChat.jsx";
-import { CartContext } from "../../../context/cart.context.jsx";
+import './Navbar.css';
+import { useContext, useState } from 'react';
+import Navbarchat from '../../chat/Navbarchat/NavbarChat.jsx';
+import { CartContext } from '../../../context/cart.context.jsx';
 
 const Navbar = () => {
-  const brands = ["Nike", "Adidas", "Puma", "Reebok", "Under Armour"];
+  const brands = ['Nike', 'Adidas', 'Puma', 'Reebok', 'Under Armour'];
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const { cart } = useContext(CartContext);
@@ -37,15 +37,15 @@ const Navbar = () => {
 
   const randomBrandIcon = () => {
     switch (randomBrand()) {
-      case "Nike":
+      case 'Nike':
         return <SiNike />;
-      case "Adidas":
+      case 'Adidas':
         return <SiAdidas />;
-      case "Puma":
+      case 'Puma':
         return <SiPuma />;
-      case "Reebok":
+      case 'Reebok':
         return <SiReebok />;
-      case "Under Armour":
+      case 'Under Armour':
         return <SiUnderarmour />;
       default:
         return <SiNike />;
@@ -63,8 +63,8 @@ const Navbar = () => {
               <a
                 className={
                   isMenuOpen
-                    ? "menu-bars-button hidden"
-                    : "menu-bars-button animate__animated animate__fadeInUp animate__faster"
+                    ? 'menu-bars-button hidden'
+                    : 'menu-bars-button animate__animated animate__fadeInUp animate__faster'
                 }
               >
                 <FaBars />
@@ -72,8 +72,8 @@ const Navbar = () => {
               <a
                 className={
                   isMenuOpen
-                    ? "menu-x-button animate__animated animate__fadeInDown animate__faster"
-                    : "menu-x-button hidden"
+                    ? 'menu-x-button animate__animated animate__fadeInDown animate__faster'
+                    : 'menu-x-button hidden'
                 }
               >
                 <FaX />
@@ -82,33 +82,33 @@ const Navbar = () => {
             <ul
               className={
                 isMenuOpen
-                  ? "nav-links mobile-nav animate__animated animate__fadeInDown animate__faster"
-                  : "hidden"
+                  ? 'nav-links mobile-nav animate__animated animate__fadeInDown animate__faster'
+                  : 'hidden'
               }
             >
               <Link
-                to={"/"}
+                to={'/'}
                 className="nav-item"
                 onClick={() => setIsMenuOpen(false)}
               >
                 <FaShop /> Produkte
               </Link>
               <Link
-                to={"/prodcuts-list"}
+                to={'/prodcuts-list'}
                 className="nav-item"
                 onClick={() => setIsMenuOpen(false)}
               >
                 <FaPercentage /> <strong>SALE</strong>
               </Link>
               <Link
-                to={"/categories"}
+                to={'/categories'}
                 className="nav-item"
                 onClick={() => setIsMenuOpen(false)}
               >
                 <BiSolidCategory /> Kategorien
               </Link>
               <Link
-                to={"/"}
+                to={'/'}
                 className="nav-item"
                 onClick={() => setIsMenuOpen(false)}
               >
@@ -122,22 +122,22 @@ const Navbar = () => {
               </div>
               <Navbarchat />
             </ul>
-            <Link className="logo" to={"/"}>
+            <Link className="logo" to={'/'}>
               FitFrenzy
             </Link>
           </div>
           <div className="nav-center">
             <ul className="nav-links pc-nav">
-              <Link to={"/products-list"} className="nav-item">
+              <Link to={'/products-list'} className="nav-item">
                 Produkte
               </Link>
-              <Link to={"/sales"} className="nav-item">
+              <Link to={'/sales'} className="nav-item">
                 <strong>SALE %</strong>
               </Link>
-              <Link to={"/categories"} className="nav-item">
+              <Link to={'/categories'} className="nav-item">
                 Kategorien
               </Link>
-              <Link to={"/products-list"} className="nav-item">
+              <Link to={'/products-list'} className="nav-item">
                 Marken
               </Link>
             </ul>
@@ -150,20 +150,20 @@ const Navbar = () => {
               </a>
             </div>
             <ul className="user-icons">
-              <Link className="nav-item" to={"/favorites"}>
-                <FaHeart />
+              <Link className="nav-item" to={'/favorites'}>
+                <FaHeart className="hover" />
               </Link>
-              <Link className="nav-item cart-icon" to={"/cart"}>
-                <FaCartShopping />
+              <Link className="nav-item cart-icon" to={'/cart'}>
+                <FaCartShopping className="hover" />
                 {cart.length !== 0 ? (
                   <span className="cart-item-count">{cart.length}</span>
                 ) : null}
               </Link>
-              <Link className="nav-item" to={"/my-orders"}>
-                <FiPackage />
+              <Link className="nav-item" to={'/my-orders'}>
+                <FiPackage className="hover" />
               </Link>
-              <Link className="nav-item" to={"/account"}>
-                <FaUser />
+              <Link className="nav-item" to={'/account'}>
+                <FaUser className="hover" />
               </Link>
             </ul>
           </div>
