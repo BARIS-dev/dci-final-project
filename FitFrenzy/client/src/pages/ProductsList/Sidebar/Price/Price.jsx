8acad585a2d84ae1d1e75e07ce755b6e5bd03c4f
BARIŞ <barishakan.dev@gmail.com>
2024-02-21 09:41:33 +0100

    few changes

--- FitFrenzy/client/src/pages/ProductsList/Sidebar/Price/Price.jsx
+++ FitFrenzy/client/src/pages/ProductsList/Sidebar/Price/Price.jsx
@@ -31,76 +31,63 @@ Prices.propTypes = {
 
 export default Prices;*/
 
+import "./Price.css";
+import Input from "../../../../components/compAndreea/Input";
+import PropTypes from "prop-types";
 
-
-import './Price.css';
-import Input from "../../../../components/compAndreea/Input"
-import PropTypes from 'prop-types';
-
-
-
-function Price({handleChange}) {
+function Price({ handleChange }) {
   return (
-    <div className='price-container'>
-      <h2 className='sidebar-title price-title'>Preis</h2>
-
+    <div className="price-container">
+      <h2 className="sidebar-title price-title">Preis</h2>
 
       <label className="sidebar-label-container">
-          <input onChange={handleChange}type="radio" value="" name="test2" />
-          <span className="checkmark"></span>All
-        </label>
+        <input onChange={handleChange} type="radio" value="" name="test2" />
+        <span className="checkmark"></span>All
+      </label>
 
-      <Input 
-      handleChange={handleChange}
-      value={50}
-      title="0- 50 €"
-      name="test2"  
+      <Input
+        handleChange={handleChange}
+        value={50}
+        title="0- 50 €"
+        name="test2"
       />
 
-      <Input 
-      handleChange={handleChange}
-      value={50}
-      title="0- 50 €"
-      name="test2"  
+      <Input
+        handleChange={handleChange}
+        value={50}
+        title="0- 50 €"
+        name="test2"
       />
 
-    <Input 
-      handleChange={handleChange}
-      value={150}
-      title="50 €-100 €"
-      name="test2"  
+      <Input
+        handleChange={handleChange}
+        value={150}
+        title="50 €-100 €"
+        name="test2"
       />
 
-      <Input 
-      handleChange={handleChange}
-      value={50}
-      title="100 €- 150€"
-      name="test2"  
+      <Input
+        handleChange={handleChange}
+        value={50}
+        title="100 €- 150€"
+        name="test2"
       />
 
-      <Input 
-      handleChange={handleChange}
-      value={200}
-      title="over 150 €"
-      name="test2"  
+      <Input
+        handleChange={handleChange}
+        value={200}
+        title="over 150 €"
+        name="test2"
       />
-      
-
-      </div>
-
+    </div>
   );
 }
 
 Price.propTypes = {
-  handleChange: PropTypes.func.isRequired
+  handleChange: PropTypes.func.isRequired,
 };
 
 export default Price;
-
-
-
-
-
 
 /*function Price() {
   return (
